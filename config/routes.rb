a44915a204324235045f1744cf67b4e7930ed8f7
Bhanu <bhanusruti512@gmail.com>
2016-09-12 18:51:34 -0700

    added welcome and contactUs views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/welcome", to:'pages#welcome'
+  get "/contact_us", to:'pages#contact_us'
 end
